67f87abe8f9bff68bc002d1e126fb4ce2ebb2126
GileardeFernandes <gileardefernandes@gmail.com>
2020-02-18 00:47:36 -0300

    Concluindo rotas deve API

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,26 +1,72 @@
-const   axios  = require('axios');
-const   Dev   = require('../models/Dev');
+const axios = require('axios');
+const Dev = require('../models/Dev');
+const parserStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
+    async index(req, res) {
+        const dev = await Dev.find();
+        return res.json(dev);
+    },
     async store(req, res) {
         const { github_username, techs, latitude, longitude } = req.body;
-        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
 
-        const { name = login, avatar_url, bio } = apiResponse.data;
-        const techsArray = techs.split(',').map(tech => tech.trim());
+        let dev = await Dev.findOne({ github_username });
 
+        if (!dev) {
+
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+            const { name = login, avatar_url, bio } = apiResponse.data;
+            const techsArray = parserStringAsArray(techs);
+
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude],
+            }
+            dev = await Dev.create({
+                name,
+                avatar_url,
+                bio,
+                github_username,
+                techs: techsArray,
+                location
+            });
+        }
+        return res.json(dev);
+    },
+
+    async update(req, res) {
+        const github_username = req.headers.github_username;
+        const { latitude, longitude, techs } = req.body;
+        
         const location = {
             type: 'Point',
             coordinates: [longitude, latitude],
         }
-        const dev = await Dev.create({
-            name,
-            avatar_url,
-            bio,
-            github_username,
-            techs: techsArray,
-            location
-        });
-        return res.json(dev);
+
+        const techsArrayUpdate = parserStringAsArray(techs);
+
+        const dev = Dev.findOne({ github_username });
+        
+       if(dev){           
+           await dev.updateOne({
+              techs: techsArrayUpdate,
+              location,
+            });         
+        }        
+
+        res.json({ code: "200ok" });
+    },
+
+    async destroy(req, res){
+        const github_username = req.headers.github_username;
+        const { latitude, longitude, techs } = req.body;
+        
+        const dev = Dev.findOne({ github_username });
+        
+        if(dev){           
+            await dev.deleteOne({ github_username });         
+         }   
+        
+         res.json({ code: "200ok" });
     }
 };
---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,29 @@
+const Dev = require('../models/Dev');
+const parserStringAsArray  = require('../utils/parseStringAsArray');
+
+module.exports = {
+  async index(req, res) {
+    const { latitude, longitude, techs } = req.query;
+    const techsArray = parserStringAsArray(techs);
+    console.log(techsArray);
+
+    const dev = await Dev.find({
+      techs:{
+        $in: techsArray
+      },
+      location:{
+        $near:{
+           $geometry:{
+             type: 'Point',
+             coordinates: [longitude, latitude]
+           },
+           $maxDistance: 10000,
+        }
+      }
+    });
+
+    return res.json(dev);
+  }
+
+
+}
